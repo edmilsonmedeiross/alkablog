0571f132e734590cd89257c63c12c76515ad0099
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-03-22 18:29:37 -0300

    :sparkles: feat: adiciona pagina de detalhe do post e seus comentários

---
+++ src/pages/[id].tsx
@@ -0,0 +1,57 @@
+import { fetchCommentsPosts } from '@/services/helpers';
+import { CommentProps } from '@/types/commentsTypes';
+import { atom, useAtom } from 'jotai';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { loadablePosts } from './index';
+
+const initialState: CommentProps[] = [];
+
+const coMmentsAtom = atom(initialState);
+
+function DetailPost() {
+  const [posts] = useAtom(loadablePosts);
+  const router = useRouter();
+  const id = typeof router.query.id === 'string' ? parseInt(router.query.id) : 0;
+  const [comments, setComments] = useAtom(coMmentsAtom);
+  
+  useEffect(() => {
+    async function fetch() {
+      const response: Array<CommentProps> = await fetchCommentsPosts(id);
+      setComments(response);
+    }
+
+    fetch();
+  }, [id]);
+  
+  if (posts.state !== 'hasData') return <div>Carregando...</div>;
+
+  const post = posts.data.find((post) => post.id === Number(id));
+
+  return (
+    <>
+      <div>DetailPost</div>
+      {post && (
+        <div>
+          <h1 className='font-medium'>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      )}
+      <section>
+        <p>Comentários</p>
+        <div>
+          {comments && comments.map((comment: CommentProps) => (
+            <div key={comment.id}>
+              <h4 className='font-bold underline'>{comment.name}</h4>
+              <p>{comment.body}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <Link href='/'>Voltar</Link>
+    </>
+  );
+}
+
+export default DetailPost;
