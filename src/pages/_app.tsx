7f388f393c17d42163258bf1d67601920d63b2a0
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-03-22 18:21:32 -0300

    :recycle: refactor: refatora

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { useState } from 'react';
+import { Provider } from 'jotai';
+import { useHydrateAtoms } from 'jotai/react/utils';
+import type { AppProps } from 'next/app';
+import { queryClientAtom } from 'jotai-tanstack-query';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+  useHydrateAtoms(new Map([[queryClientAtom, queryClient]]));
+
+  return (
+    <QueryClientProvider client={ queryClient }>
+      <Hydrate state={ pageProps.dehydratedState }>
+        <Provider>
+          <Component {...pageProps} />
+        </Provider>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
