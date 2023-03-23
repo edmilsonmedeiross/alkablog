a9cb075e22fca77dd1800efc7cb4d3316fd0c7c3
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-03-23 02:50:41 -0300

    :lipstick: feat: estiliza header e home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../../public/next.svg';
+import { FaHome } from 'react-icons/fa';
+import { FaBookReader } from 'react-icons/fa';
 
 function Header() {
   return (
-    <div>
-      <div>
+    <div className='flex justify-between fle w-full h-40 pl-4 bg-slate-500'>
+      <div className='flex'>
         <Image
+          className=''
           src={logo}
           alt="logo"
           width={200}
@@ -14,9 +17,28 @@ function Header() {
           priority={true}
         />
       </div>
-      <nav>
-        <Link href='#users'>Top Leitores</Link>
-        <Link href='/'>Home</Link>
+      <nav className='flex justify-center items-center gap-3 pr-4 font-bold text-slate-900'>
+        <Link
+          href='#users'
+          className='
+            hover:text-blue-100 hover:font-bold
+            transition duration-300 ease-in-out
+            flex items-center gap-2'
+        >
+          <FaBookReader size={25}/>
+          Top Leitores
+        </Link>
+        <Link
+          href='/'
+          className='
+            hover:text-blue-100
+            hover:font-bold transition
+            duration-300 ease-in-out
+            flex items-center gap-2'
+        >
+          <FaHome size={25}/>
+          Home
+        </Link>
       </nav>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,15 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { UserProps } from '@/types/userTypes';
 import Header from '@/components/Header';
+import { 
+  FaAngleRight, FaAngleDoubleRight,
+  FaAngleLeft, FaAngleDoubleLeft
+} from 'react-icons/fa';
 
 const ONE = 1;
 const PAGE_MAX = 10;
 
-
+// cria átomos do jotai
 export const usersAtom = atom([] as UserProps[]);
 const isNextVisibleAtom = atom(true);
 const isPrevVisibleAtom = atom(false);
@@ -47,9 +51,9 @@ export default function Home() {
   useEffect(() => {
     async function fetch() {
       const users = await fetchUsers();
+      // gera uma url aleatória para o avatar do usuário com o robohash
+      // https://robohash.org/
       users.forEach((user) => {
-        // gera uma url aleatória para o avatar do usuário com o robohash
-        // https://robohash.org/
         const text = Math.random().toString(36).substring(7);
         user.avatar = `https://robohash.org/${text}.png?size=200x200`;
       });
@@ -65,47 +69,77 @@ export default function Home() {
     <>
       <Header />
       <main>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <div>
-          <article className='mx-auto p-4' >
+        <div className='w-full h-full mt-20'>
+          <article className='p-4 flex flex-wrap gap-5 justify-center'>
             {posts.state === 'hasData' && posts.data.map((post) => (
-              <Link href={`/post/${post.id}`} key={ post.id } >
-                <div className='my-px'>
-                  <h1 className='font-bold underline'>{ post.title }</h1>
+              <Link
+                href={`/post/${post.id}`}
+                key={ post.id }
+                className='flex flex-col justify-center items-center
+                 w-72 border-solid border-2 border-gray-400 rounded-md
+                  hover:bg-gray-200 transition duration-300 ease-in-out text-center
+                  bg-gray-100 h-72 p-5 mt-3'
+              >
+                <div className='flex flex-col items-center gap-8'>
+                  <h1 className='font-bold'>{ post.title }</h1>
                   <p>{ post.body }</p>
                 </div>
               </Link>
             ))}
           </article>
         </div>
+        <div className='flex justify-between'>
+          <div className='ml-8'>
+            {isPrevVisible && <button onClick={() => setPage(1) } ><FaAngleDoubleLeft size={30}/></button>}
+            {isPrevVisible && <button onClick={() => setPage(page - 1)} ><FaAngleLeft size={30}/></button>}
+          </div>
+          <div className='mr-8'>
+            {isNextVisible && <button onClick={() => setPage(page + 1) } ><FaAngleRight size={30}/></button>}
+            {isNextVisible && <button onClick={() => setPage(allPosts.length / PAGE_MAX) } ><FaAngleDoubleRight size={30}/></button>}
+          </div>
+        </div>
         <div>
-          <section>
+          <h3 
+            className='
+              font-bold text-2xl text-center mt-8 mb-8'
+          >
+            Top Leitores do Mês
+          </h3>
+          <section className='flex flex-auto flex-wrap gap-3'>
             {
               users.map((user: UserProps) => (
                 <Link
-                  key={user.id} href={`user/${user.id}`}
+                  className='
+                    w-36 h-36 rounded-full flex
+                    flex-col gap-y-8 items-center justify-center
+                    bg hover:transform hover:scale-110
+                    transition-transform duration-300 ease-in-out
+                    mx-auto'
+                  key={user.id}
+                  href={`user/${user.id}`}
                   id='users'
                 >
-                  <div>
-                    <h3>{user.username}</h3>
+                  <div className='flex flex-col gap-3 items-center'>
                     <Image
+                      className='rounded-full object-fill'
                       src={user.avatar}
                       alt={user.username}
-                      width={200}
-                      height={200}
+                      width={70}
+                      height={70}
                     />
+                    <p 
+                      className='
+                        text-center text-xs font-semibold hover:text-blue-600
+                        transition-colors duration-300'
+                    >
+                      {user.username}
+                    </p>
                   </div>
                 </Link>
               ))
             }
           </section>
         </div>
-        {isPrevVisible && <button onClick={() => setPage(page - 1)} >Prev</button>}
-        {isNextVisible && <button onClick={() => setPage(page + 1) } >Next</button>}
-        {isNextVisible && <button onClick={() => setPage(allPosts.length / PAGE_MAX) } >Ultima</button>}
-        {isPrevVisible && <button onClick={() => setPage(1) } >Primeira</button>}
       </main>
     </>
   );
